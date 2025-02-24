2af9333d47a7155dc3be39950c78ca0ddebad2e8
ml3424 <ml3424@bs.amalnet.k12.il>
2025-02-24 15:38:06 +0200

    java doc in credits

--- app/src/main/java/com/example/ex1101_shared_preferences/Credits.java
+++ app/src/main/java/com/example/ex1101_shared_preferences/Credits.java
@@ -19,12 +19,22 @@ public class Credits extends AppCompatActivity {
 
     }
 
+    /**
+     * creats menu
+     * @param	menu Menu
+     * @return	none
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    /**
+     * handles menu item selection
+     * @param	item MenuItem
+     * @return	none
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         String st = item.getTitle().toString();
